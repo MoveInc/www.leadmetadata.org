fc5221e1a22ca2d25b82d616744a82058e54056d
Rusty Klophaus <rklophaus@gmail.com>
2014-12-15 10:54:34 -0500

    Update the deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env sh
 
 BUCKET=s3://www.leadmetadata.org
-
-aws s3 cp index.html     $BUCKET
-aws s3 cp normalize.css  $BUCKET
-aws s3 cp skeleton.css   $BUCKET
-aws s3 cp style.css      $BUCKET
+aws s3 sync . $BUCKET --exclude ".git/*"
